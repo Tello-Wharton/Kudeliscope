48156abb7cba82bd254f4b341de9974b8d1748a8
Isabelle Barrett <isabelle.barrett@kcl.ac.uk>
2017-10-21 22:32:29 +0100

    Emotion recognition API in python 2.7

---
+++ EmotionAPI.py
@@ -0,0 +1,26 @@
+import httplib, urllib, base64
+
+headers = {
+    # Request headers. Replace the placeholder key below with your subscription key.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': 'f1f0083849dd4dc1a2858e8940be0537',
+}
+
+params = urllib.urlencode({
+})
+
+# Replace the example URL below with the URL of the image you want to analyze.
+body = "{ 'url': 'http://kudeliscope.com/img/ged.png' }"
+
+try:
+    # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+    #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
+    #   URL below with "westcentralus".
+    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
